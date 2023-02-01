e2b263b19d419a9a0c30f81aa69fb5cbc3be84e6
SVitaly <user7_490@mail.ru>
2023-02-01 20:50:06 +0300

    добавлено решение задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Сколько чисел будем проверять?");
+int count = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Вводите числа:");
+int number = 0;
+int result = 0;
+for (int i = 0; i < count; i++)
+    {
+    number = Convert.ToInt32(Console.ReadLine());
+    if (number > 0)  result += 1;
+    }
+Console.WriteLine ($"Вы ввели {result} чисел больше нуля");
